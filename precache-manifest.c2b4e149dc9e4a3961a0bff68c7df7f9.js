f361478ad2e848270b43d0e24de7f4e2bb92e45b
Tim Bastiaans <Tim_bastiaans@hotmail.nl>
2021-05-25 22:09:44 +0200

    Updates

---
+++ precache-manifest.c2b4e149dc9e4a3961a0bff68c7df7f9.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "661a4ecfa30035aef6afe7b89d84d790",
+    "url": "/Timbastiaans.github.io/Portofolio/index.html"
+  },
+  {
+    "revision": "9bc792ea54302df30680",
+    "url": "/Timbastiaans.github.io/Portofolio/static/css/3.ed673b97.chunk.css"
+  },
+  {
+    "revision": "84e4fae5c2161147a8f7",
+    "url": "/Timbastiaans.github.io/Portofolio/static/css/4.0110b6a7.chunk.css"
+  },
+  {
+    "revision": "25c8e3dd75060c1ad9ba",
+    "url": "/Timbastiaans.github.io/Portofolio/static/css/main.abc29259.chunk.css"
+  },
+  {
+    "revision": "b90634f6789c477f3948",
+    "url": "/Timbastiaans.github.io/Portofolio/static/js/2.6f2ae8cd.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "/Timbastiaans.github.io/Portofolio/static/js/2.6f2ae8cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bc792ea54302df30680",
+    "url": "/Timbastiaans.github.io/Portofolio/static/js/3.7e71581d.chunk.js"
+  },
+  {
+    "revision": "84e4fae5c2161147a8f7",
+    "url": "/Timbastiaans.github.io/Portofolio/static/js/4.8e938c31.chunk.js"
+  },
+  {
+    "revision": "25c8e3dd75060c1ad9ba",
+    "url": "/Timbastiaans.github.io/Portofolio/static/js/main.2c52e83d.chunk.js"
+  },
+  {
+    "revision": "0b18a097dabde3c20f47",
+    "url": "/Timbastiaans.github.io/Portofolio/static/js/runtime-main.1700e0ea.js"
+  },
+  {
+    "revision": "f2a4602783889007136a88b7f6552a0f",
+    "url": "/Timbastiaans.github.io/Portofolio/static/media/AWS-Logo.f2a46027.jpg"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/Timbastiaans.github.io/Portofolio/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "836a943b857a442b844359847c24a084",
+    "url": "/Timbastiaans.github.io/Portofolio/static/media/BRTHRS-logo.836a943b.png"
+  },
+  {
+    "revision": "f5df85f01e0b668378fab4f91dcea32b",
+    "url": "/Timbastiaans.github.io/Portofolio/static/media/CCNA-Routing-and-Switching-Certification.f5df85f0.jpg"
+  },
+  {
+    "revision": "afd6b23524b82947cdcb5cc298928e89",
+    "url": "/Timbastiaans.github.io/Portofolio/static/media/HAN-logo.afd6b235.png"
+  },
+  {
+    "revision": "7a6fd67d764cc3174202971c29cdaa9f",
+    "url": "/Timbastiaans.github.io/Portofolio/static/media/HYSTER-YALE-logo.7a6fd67d.png"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/Timbastiaans.github.io/Portofolio/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "f361b60553dc3e086aaacef3aa845256",
+    "url": "/Timbastiaans.github.io/Portofolio/static/media/VTS-logo.f361b605.png"
+  },
+  {
+    "revision": "3cab17473e12524e0dbb480b484b7a8a",
+    "url": "/Timbastiaans.github.io/Portofolio/static/media/beerDistrubutionGame.3cab1747.png"
+  },
+  {
+    "revision": "7c66adb56c500d107a6cba0410303acd",
+    "url": "/Timbastiaans.github.io/Portofolio/static/media/bubble-trouble -logo.7c66adb5.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/Timbastiaans.github.io/Portofolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/Timbastiaans.github.io/Portofolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/Timbastiaans.github.io/Portofolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/Timbastiaans.github.io/Portofolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/Timbastiaans.github.io/Portofolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
